cb2354d7efe868d09bc3409212bbc3f12a7a89b4
NoaRAMIA <noa.ramia@limayrac.fr>
2025-10-09 10:51:30 +0200

    Page validation de la commande
    Page validation de la commande

---
+++ Site/validation.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Validation</title>
+</head>
+<body>
+    <div class="funds-success-message-container">
+  <div class="funds-checkmark-text-container">
+    <div class="funds-checkmark-container">
+      
+      <svg class="funds-checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="funds-checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path class="funds-checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg> 
+      
+     <div class="funds-display-on-ie">
+       <svg class="funds-ie-checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="funds-ie-checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path class="funds-ie-checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg> 
+      </div>
+    </div>
+    
+    <h1 class="funds-success-done-text">Validé ! </h1>
+  </div>
+  
+  <div class="funds-success-message">
+    
+      
+    <h2>Votre commande à bien été prise en compte.  </h2>
+    <p>Votre commande nᵒ 123456 a bien été prise en compte, un mail vous sera transmis afin que vous veniez la récupérer. Merci de votre confiance, Maison Wagyu. </p>
+      <button><a href="./index.php"> Accueil </a> </button> 
+    
+  </div>
+  
+  
+</div>
+    
+</body>
+</html>
