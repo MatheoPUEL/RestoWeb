4593e5e58edbe731e63bc82958dd8be0e8c8d704
MatheoPUEL <puel.matheo@gmail.com>
2025-10-16 09:10:12 +0200

    Ajout de la commande dans la db fonctionnel

--- Site/function/pushCommande.php
+++ Site/function/pushCommande.php
@@ -1,28 +1,46 @@
 <?php
+require_once('../classes/panier.classes.php');
 session_start();
 require_once('db_function.php');
 $dbh = db_connect();
-if (isset($_SESSION['emailUtilisateur']) && isset($_SESSION['idUtilisateur'])) {
 
-    $CreateEmptyCommand = "INSERT INTO commande (dateCommande, totalCommande, typeCommande, idEtat, idUtilisateur)
-VALUES (NOW(), :totalCommande, :typeCommande, :idEtat, :idUtilisateur)";
+if ( isset($_SESSION['panier']) && isset($_SESSION['emailUtilisateur']) && isset($_SESSION['idUtilisateur'])) {
 
-    $Initer = $dbh->prepare($CreateEmptyCommand);
+    $CreateCommande = "INSERT INTO commande (dateCommande, totalCommande, typeCommande, idEtat, idUtilisateur)
+                       VALUES (NOW(), :totalCommande, :typeCommande, :idEtat, :idUtilisateur)";
+    $stmt = $dbh->prepare($CreateCommande);
 
-    $Initer->execute([
-        ':totalCommande' => 0,
+    $total = 0;
+    foreach ($_SESSION['panier'] as $produit) {
+        $total += $produit->prix;
+    }
+
+    $stmt->execute([
+        ':totalCommande' => $total,
         ':typeCommande' => 0,
-        ':idEtat' => 1, 
+        ':idEtat' => 1,
         ':idUtilisateur' => $_SESSION['idUtilisateur']
     ]);
 
-    echo "Commande initialisée avec succès.";
+    $idCommande = $dbh->lastInsertId();
+
+    $InsertLigne = "INSERT INTO ligne_commande (idCommande, idProduit, qteCommandee, prixHtLigneCommande)
+                    VALUES (:idCommande, :idProduit, :quantite, :prix)";
+    $stmtLigne = $dbh->prepare($InsertLigne);
+
+    foreach ($_SESSION['panier'] as $produit) {
+        $stmtLigne->execute([
+            ':idCommande' => $idCommande,
+            ':idProduit' => $produit->id,
+            ':quantite' => 1,
+            ':prix' => $produit->prix
+        ]);
+    }
+    $_SESSION['panier'] = null;
+    echo $_SESSION['panier'];
+    echo "Commande et lignes enregistrées avec succès.";
 
 } else {
-    echo "Connectez-vous pour passer une commande.";
-}
-
-function eachProduct() {
-    
+    echo "Veuillez vous connecter.";
 }
 ?>
